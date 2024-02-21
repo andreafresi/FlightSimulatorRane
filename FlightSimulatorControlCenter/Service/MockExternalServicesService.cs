7b995c8a882f1d54af1c10e6401ce461dacb7d91
Lorenzo Tagliani <tagliani46@gmail.com>
2024-02-21 12:45:32 +0100

    Agg bottone annulla in FleetManager e MockExternalServicesService

---
+++ FlightSimulatorControlCenter/Service/MockExternalServicesService.cs
@@ -0,0 +1,70 @@
+﻿using Clients.ImpiantiClient;
+using FlightSimulatorControlCenter.Service.Int;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorControlCenter.Service
+{
+    internal class MockExternalServicesService : IExternalServicesService
+    {
+        private Client _clientFlightSimulator { get; set; }
+
+        public MockExternalServicesService(string baseAdress)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(baseAdress);
+            _clientFlightSimulator = new(client);
+        }
+
+        public FlottaApi FlottaPOSTAsync(CreateFlottaRequest req)
+        {
+            List<AereoApi> aerei = new List<AereoApi>() {
+            new AereoApi() { IdAereo = 1, CodiceAereo = "AereoCod1", Colore = "Rosso", NumeroDiPosti = 10 },
+            new AereoApi() { IdAereo = 2, CodiceAereo = "AereoCod1", Colore = "Rosso", NumeroDiPosti = 10 },
+            new AereoApi() { IdAereo = 3, CodiceAereo = "AereoCod1", Colore = "Rosso", NumeroDiPosti = 10 },
+            };
+
+            var flottaCreataApi = (_clientFlightSimulator.FlottaPOSTAsync(req)).Result;
+            return flottaCreataApi;
+        }
+
+        public List<FlottaApi> GetElencoFlotteAsync()
+        {
+            List<AereoApi> aerei = new List<AereoApi>() {
+            new AereoApi() { IdAereo = 1, CodiceAereo = "AereoCod1", Colore = "Rosso", NumeroDiPosti = 10 },
+            new AereoApi() { IdAereo = 2, CodiceAereo = "AereoCod1", Colore = "Rosso", NumeroDiPosti = 10 },
+            new AereoApi() { IdAereo = 3, CodiceAereo = "AereoCod1", Colore = "Rosso", NumeroDiPosti = 10 },
+            };
+
+            var elencoFlotte = (_clientFlightSimulator.GetElencoFlotteAsync()).Result;
+            return elencoFlotte.ToList();
+        }
+
+        public FlottaApi GetFlottaAsync(long idFLotta)
+        {
+            List<AereoApi> aerei = new List<AereoApi>() {
+            new AereoApi() { IdAereo = 1, CodiceAereo = "AereoCod1", Colore = "Rosso", NumeroDiPosti = 10 },
+            new AereoApi() { IdAereo = 2, CodiceAereo = "AereoCod1", Colore = "Rosso", NumeroDiPosti = 10 },
+            new AereoApi() { IdAereo = 3, CodiceAereo = "AereoCod1", Colore = "Rosso", NumeroDiPosti = 10 },
+            };
+
+            var flotta = (_clientFlightSimulator.FlottaGETAsync(idFLotta)).Result;
+            return flotta;
+        }
+
+        public AereoApi AereoPOSTAsync(CreateAereoRequest req)
+        {
+            var aereoCreatoApi = (_clientFlightSimulator.AereoPOSTAsync(req)).Result;
+            return aereoCreatoApi;
+        }
+
+        public AereoApi AereoPutAsync(UpdateAereoRequest req)
+        {
+            var aereoModificatoApi = (_clientFlightSimulator.AereoPUTAsync(req)).Result;
+            return aereoModificatoApi;
+        }
+    }
+}
